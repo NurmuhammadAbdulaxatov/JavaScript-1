257bf1b9d1942dbf186fa28ee74aab6b69c3d0b5
Nurmuhammad <abdulaxatov3@gmail.com>
2022-11-18 16:14:36 +0500

    Java Script 1-dars

---
+++ main.js
@@ -0,0 +1,19 @@
+confirm("Saytga xush kelibsiz! Qoidalar bilan tanishib chiqing!");
+
+var userName = prompt("Ismingiz?");
+console.log("Ism: " + userName);
+
+var userLastName = prompt("Familiyangiz?");
+console.log("Familiya: " + userLastName);
+
+var userAge = prompt("Yoshingiz");
+console.log("Yosh: " + userAge);
+
+var userJob = prompt("Kasbingiz");
+console.log("Kasb: " + userJob);
+
+var userInterest = prompt("Qiziqishingiz:");
+console.log("Qiziqish: " + userInterest);
+
+confirm("Siz ajoyibsiz! Biz bilan ishlashga rozimisiz");
+confirm("3 ball qoyarsiz :)");
